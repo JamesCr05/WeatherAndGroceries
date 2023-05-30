5228a6e8134c277e1b341ea94a135a0994b68c3c
James Crabtree <107822712+JamesCr05@users.noreply.github.com>
2023-05-29 21:48:44 -0500

    Add files via upload

---
+++ lib/weather/weather_service.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NetworkData {
+  NetworkData(this.url);
+
+  final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    }
+  }
+}
+
+class WeatherModel {
+  Future<dynamic> getLocationWeather(double latitude, double longitude) async {
+    NetworkData networkHelper = NetworkData(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid={INSERT_API_KEY}&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getDailyForecast(double latitude, double longitude) async {
+    NetworkData networkHelper = NetworkData(
+        'https://api.openweathermap.org/data/2.5/forecast?lat=$latitude&lon=$longitude&appid={INSERT_API_KEY}&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+}
